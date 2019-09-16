f348a90f24976b9b37f1ce5a40dfdcbde8f0a8f6
Lennart Oldenburg <hello@lennartoldenburg.de>
2019-09-16 15:00:03 +0200

    Added GCP request templates and further structure to runner.

--- cmd/operator/runner.go
+++ cmd/operator/runner.go
@@ -1,5 +1,122 @@
 package main
 
+var tmplInstanceCreate = `{
+  "kind": "compute#instance",
+  "name": "ACS_EVAL_INSERT_GCP_MACHINE_NAME",
+  "zone": "ACS_EVAL_INSERT_ZONE",
+  "minCpuPlatform": "ACS_EVAL_INSERT_MIN_CPU_PLATFORM",
+  "machineType": "ACS_EVAL_INSERT_MACHINE_TYPE",
+  "displayDevice": {
+    "enableDisplay": false
+  },
+  "metadata": {
+    "kind": "compute#metadata",
+    "items": [
+      {
+        "key": "nameOfNode",
+        "value": "ACS_EVAL_INSERT_NAME"
+      },
+      {
+        "key": "partnerOfNode",
+        "value": "ACS_EVAL_INSERT_PARTNER"
+      },
+      {
+        "key": "typeOfNode",
+        "value": "ACS_EVAL_INSERT_TYPE_OF_NODE"
+      },
+      {
+        "key": "resultFolder",
+        "value": "ACS_EVAL_INSERT_RESULT_FOLDER"
+      },
+      {
+        "key": "evalScriptToPull",
+        "value": "ACS_EVAL_INSERT_EVAL_SCRIPT_TO_PULL"
+      },
+      {
+        "key": "binaryToPull",
+        "value": "ACS_EVAL_INSERT_BINARY_TO_PULL"
+      },
+      {
+        "key": "tcConfig",
+        "value": "ACS_EVAL_INSERT_TC_CONFIG"
+      },
+      {
+        "key": "killZenoMixesInRound",
+        "value": "ACS_EVAL_INSERT_KILL_ZENO_MIXES_IN_ROUND"
+      },
+      {
+        "key": "pkiIP",
+        "value": "ACS_EVAL_INSERT_PKI_IP"
+      },
+      {
+        "key": "startup-script-url",
+        "value": "ACS_EVAL_INSERT_STARTUP_SCRIPT"
+      }
+    ]
+  },
+  "tags": {
+    "items": [
+      "ACS_EVAL_INSERT_TAG"
+    ]
+  },
+  "disks": [
+    {
+      "kind": "compute#attachedDisk",
+      "type": "PERSISTENT",
+      "boot": true,
+      "mode": "READ_WRITE",
+      "autoDelete": true,
+      "deviceName": "ACS_EVAL_INSERT_GCP_MACHINE_NAME",
+      "initializeParams": {
+        "sourceImage": "ACS_EVAL_INSERT_SOURCE_IMAGE",
+        "diskType": "ACS_EVAL_INSERT_DISK_TYPE",
+        "diskSizeGb": "ACS_EVAL_INSERT_DISK_SIZE"
+      },
+      "diskEncryptionKey": {}
+    }
+  ],
+  "canIpForward": false,
+  "networkInterfaces": [
+    {
+      "kind": "compute#networkInterface",
+      "subnetwork": "ACS_EVAL_INSERT_SUBNETWORK",ACS_EVAL_INSERT_ACCESS_CONFIG
+      "aliasIpRanges": []
+    }
+  ],
+  "description": "",
+  "labels": {},
+  "scheduling": {
+    "preemptible": false,
+    "onHostMaintenance": "TERMINATE",
+    "automaticRestart": false,
+    "nodeAffinities": []
+  },
+  "deletionProtection": false,
+  "serviceAccounts": [
+    {
+      "email": "ACS_EVAL_INSERT_SERVICE_ACCOUNT",
+      "scopes": [
+        "https://www.googleapis.com/auth/servicecontrol",
+        "https://www.googleapis.com/auth/service.management.readonly",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring.write",
+        "https://www.googleapis.com/auth/trace.append",
+        "https://www.googleapis.com/auth/devstorage.full_control"
+      ]
+    }
+  ]
+}`
+
+var tmplInstancePublicIP = `
+      "accessConfigs": [
+        {
+          "kind": "compute#accessConfig",
+          "name": "External NAT",
+          "type": "ONE_TO_ONE_NAT",
+          "networkTier": "PREMIUM"
+        }
+      ],`
+
 // RunExperiments is the authoritative goroutine
 // for provisioning machines and conducting all
 // experiments queued in the Operator.
@@ -8,11 +125,23 @@ func (op *Operator) RunExperiments() {
 	for {
 
 		// Wait for new experiment to be queued.
-		exp := <-op.PublicChan
+		expID := <-op.PublicChan
 
 		op.Lock()
-		op.ExpInProgress = exp
+
+		// Mark this experiment as in progress.
+		op.ExpInProgress = expID
+
+		// Stash all experiment data.
+		exp := op.Exps[expID]
+
 		op.Unlock()
+
+		if exp.System == "zeno" {
+
+			// If zeno is being evaluated, first start
+			// the PKI process in background.
+		}
 
 		// TODO: Spawn all server machines.
 
@@ -26,10 +155,17 @@ func (op *Operator) RunExperiments() {
 
 		// TODO: Wait for all clients to signal completion.
 
-		op.Exps[exp].Concluded = true
+		exp.Concluded = true
 
 		op.Lock()
+
+		// Reset in-progress indicator.
 		op.ExpInProgress = ""
+
+		// Overwrite experiment values in Operator
+		// struct in case they did not update.
+		op.Exps[expID] = exp
+
 		op.Unlock()
 	}
 }
